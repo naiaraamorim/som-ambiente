80152242c6abe20e7847e2efb5fc8c7692e4ccc4
Naiara Amorim <naiarataianelimaamorim@gmail.com>
2025-10-18 16:21:15 -0300

    [FEATURE

--- src/components/pomodoro.tsx
+++ src/components/pomodoro.tsx
@@ -1,14 +1,25 @@
 import { Play, Pause, Undo2 } from 'lucide-react'
 
 export default function Pomodoro() {
+  function play(){
+    alert("Pomodoro started")
+  }
+
+  function pause(){
+    alert("Pomodoro paused")
+  }
+
+  function restart(){
+    alert("Pomodoro restarted")
+  }
 
   return (
     <div className="flex flex-col">
           <div className="container px-4 py-8">
             <h1 className="">
-              <Play className="inline-block mr-2" />
-              <Pause className="inline-block mr-2" />
-              <Undo2 className="inline-block mr-2" />
+              <Play className="inline-block mr-2" onClick={play} />
+              <Pause className="inline-block mr-2" onClick={pause} />
+              <Undo2 className="inline-block mr-2" onClick={restart} />
             </h1>
           </div>
     </div>
